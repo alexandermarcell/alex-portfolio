bb223182e2ece752cfc678baeee389880a1359ad
alexandrmarcell <alexandermarcell36@gmail.com>
2022-05-27 12:54:29 -0400

    experimenting with opening external link

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -24,7 +24,7 @@ function Work() {
             {projects.map((project) => {
               return(
                 <div className='work__project' key={project.id}>
-                  <img src={project.image} alt='' className="work__image" onClick={() => window.open(project.link)} />
+                  <img src={project.image} alt='' className="work__image" onClick={() => window.open(project.link, '_blank')} />
                   <h2 className="work__projectName">
                     {project.title}</h2>
                   <p className="work__info">
