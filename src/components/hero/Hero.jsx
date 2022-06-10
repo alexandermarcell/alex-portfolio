e89cd5a4fb7611f94a7a53eac4242b57ee6218b6
alexandrmarcell <alexandermarcell36@gmail.com>
2022-06-10 17:42:24 -0400

    test2

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,12 +4,11 @@ import heroImage from '../../assets/image/hero-image.jpg';
 const Hero = () => {
   return (
     <div className="hero" id='hero' 
-    style={{ backgroundImage: `url(${heroImage})`, width: '100%', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center center', height: '55vh', linearGradient: 'rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.9)', border:'none !important' }}
+    style={{ backgroundImage: `url(${process.env.PUBLIC_URL + heroImage})`, width: '100%', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center center', height: '55vh', linearGradient: 'rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.9)', border:'none !important' }}
     >
       <div className="hero__wrap">
         <h1 className="hero__text">
-          Hi. I'm Alexander,
-          A Web Developer.
+          Hi. I'm Alexander.
         </h1>
       </div>
     </div>
