e7de7e6ef074da2fdb02d33eb67239642ef4ef2a
alexandrmarcell <alexandermarcell36@gmail.com>
2022-07-08 12:52:13 -0400

    final commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -32,7 +32,6 @@ function Contact() {
     emailjs.sendForm('service_cshbo9b', 'template_b3181om', e.target, 'vh3TBm5FAk2l0lQFv')
       .then((result) => {
           console.log(result.text);
-          location.reload();
           window.location.reload();
       }, (error) => {
           console.log(error.text);
